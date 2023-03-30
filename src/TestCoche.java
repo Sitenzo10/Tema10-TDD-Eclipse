462dabc4d7f0d8db1979bad2b80370090950eb59
andres <andres10penalva@gmail.com>
2023-03-30 19:52:36 +0200

    segundo test Eclipse

--- src/TestCoche.java
+++ src/TestCoche.java
@@ -1,12 +1,14 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class TestCoche {
 
 	@Test
-	public void test_crear_coche() {
+	public void test_crear_coche_vel_cero() {
 		Coche nuevoCoche = new Coche();
+		Assertions.assertEquals(0, nuevoCoche.velocidad);
 	
 	}
 
